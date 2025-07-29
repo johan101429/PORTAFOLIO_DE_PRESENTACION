0577d19ed86c60070e9538c57b1bc1de83a12e6f
johan101429 <jona1014.mr@gmail.com>
2025-07-28 22:30:17 -0500

    se corrige error en la parte de  route

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,24 +6,23 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: Request) {
   try {
     const dataForm = await req.json();
-    try {
-      const data = await resend.emails.send({
-        from: "Acme <onboarding@resend.dev>",
-        to: ["jona1014.mr@gmail.com"],
-        subject: "Nuevo mensaje de contacto desde tu portafolio",
-        react: await EmailTemplate({
-          firstName: dataForm.username,
-          message: dataForm.message,
-          email: dataForm.email,
-        }),
-        text: "Johan Mauricio Gil",
-      });
-      return Response.json({ message: 'Mensaje enviado correctamente', data }, { status: 200 });
-    } catch (error) {
-      // podrías agregar un console.error si quieres loggear internamente
-      return Response.json({ error: 'Error al enviar email' }, { status: 500 });
-    }
+
+    const data = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["jona1014.mr@gmail.com"],
+      subject: "Nuevo mensaje de contacto desde tu portafolio",
+      react: await Promise.resolve(EmailTemplate({
+        firstName: dataForm.username,
+        message: dataForm.message,
+        email: dataForm.email,
+      })),
+      text: "Johan Mauricio Gil",
+    });
+
+    return Response.json({ message: 'Mensaje enviado correctamente', data }, { status: 200 });
+
   } catch (error) {
-    return Response.json({ error: 'Error al procesar la solicitud' }, { status: 500 });
+    console.error('Error al enviar mensaje:', error);
+    return Response.json({ error: 'Ocurrió un error al procesar o enviar el mensaje.' }, { status: 500 });
   }
 }
