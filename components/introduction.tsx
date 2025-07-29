927dcf119e76e66daee2de144627eebb11b48190
johan101429 <jona1014.mr@gmail.com>
2025-07-28 22:20:07 -0500

    se corrige error en la parte de introduccion

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -6,28 +6,38 @@ import styles from "./introduction.module.css";
 import Container from "./shared/container";
 
 const Introduction = () => {
-
-    return (
-        <Container>
-            <div className={styles.home} id="home">
-                <h3 className={styles.greeting}>Hello I'm</h3>
-                <h1 className={styles.title}>JOHAN MAURICIO GIL RENDON</h1>
-                <h2 className={styles.subtitle}>Frontend Developer</h2>
-                <div className={styles.flexItems}>
-                    <div className={styles.buttonGroup}>
-                        <Link className={buttonVariants()} href="#contact">
-                            <Mail className="mr-2" /> Contact Me
-                        </Link>
-                        <Link className={buttonVariants({ variant: "secondary" })} href="/cv-johan-gil.pdf" target="_blank">
-                            <Paperclip className="mr-2" /> Download CV
-                        </Link>
-                    </div>
-                </div>
-                <Image src="/foto.jpg" alt="Johan Gil" width={180} height={180} className={styles.avatar} />
-            </div>
-
-        </Container>
-    );
+  return (
+    <Container>
+      <div className={styles.home} id="home">
+        <h3 className={styles.greeting}>Hello I'm</h3>
+        <h1 className={styles.title}>JOHAN MAURICIO GIL RENDON</h1>
+        <h2 className={styles.subtitle}>Frontend Developer</h2>
+        <div className={styles.flexItems}>
+          <div className={styles.buttonGroup}>
+            <Link className={buttonVariants()} href="#contact">
+              <Mail className="mr-2" /> Contact Me
+            </Link>
+            <Link
+              className={buttonVariants({ variant: "secondary" })}
+              href="/cv-johan-gil.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Paperclip className="mr-2" /> Download CV
+            </Link>
+          </div>
+        </div>
+        <Image
+          src="/foto.jpg"
+          alt="Johan Gil"
+          width={180}
+          height={180}
+          className={styles.avatar}
+          priority
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default Introduction;
